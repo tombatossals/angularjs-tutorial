3de3cd47a667953b4f024f9281c558f608984b62
tombatossals <david.rubert@gmail.com>
2013-07-22 23:51:57 +0200

    Phase 2 already done

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,20 +1,54 @@
 (function() {
     "use strict";
 
-    var app = angular.module("HelloWorldApp", []);
+    var app = angular.module("HelloWorldApp", [], function($routeProvider, $locationProvider) {
+        $routeProvider.when("/area", {
+            templateUrl: 'templates/area.html',
+            controller: 'AreaController'
+        });
 
-    app.controller("HelloWorldController", [ "$scope", function($scope) {
+        $routeProvider.when("/perimeter", {
+            templateUrl: 'templates/perimeter.html',
+            controller: 'PerimeterController'
+        });
+
+        $locationProvider.html5Mode(false);
+    });
+
+    app.controller("HelloWorldController", [ "$scope", "$route", "$routeParams", "$location", function($scope, $route, $routeParams, $location) {
+        $scope.$route = $route;
+        $scope.$location = $location;
+        $scope.$routeParams = $routeParams;
+        $scope.circleArea = "hola";
+
         $scope.numberError = false;
-        $scope.circlePerimeter = function() {
-            return 2 * Math.PI * $scope.perimeter;
+
+        $scope.circleArea = function() {
+            console.log($scope);
+            return Math.PI * Math.pow($scope.number, 2);
         };
 
         $scope.$watch("number", function(newValue, oldValue) {
-            if (!newValue typeof newValue !== typeof 1) {
+            if (newValue === undefined) return;
+
+            if (isNaN(newValue)) {
                 $scope.numberError = true;
             } else {
                 $scope.numberError = false;
             }
         });
     }]);
+
+    app.controller("AreaController", [ "$scope", "$routeParams", function($scope, $routeParams) {
+        $scope.circleArea = function() {
+            return Math.PI * Math.pow($scope.number, 2);
+        };
+    }]);
+
+    app.controller("PerimeterController", [ "$scope", "$routeParams", function($scope, $routeParams) {
+        $scope.circlePerimeter = function() {
+            return 2 * Math.PI * $scope.number;
+        };
+    }]);
+
 }());
