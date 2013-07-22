d66dd9a352dec171a9b5daf709ed32e33f0c896a
David Rubert <david.rubert@gmail.com>
2013-07-22 18:39:36 +0200

    Avances en la demo

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -4,8 +4,17 @@
     var app = angular.module("HelloWorldApp", []);
 
     app.controller("HelloWorldController", [ "$scope", function($scope) {
+        $scope.numberError = false;
         $scope.circlePerimeter = function() {
             return 2 * Math.PI * $scope.perimeter;
         };
+
+        $scope.$watch("number", function(newValue, oldValue) {
+            if (!newValue typeof newValue !== typeof 1) {
+                $scope.numberError = true;
+            } else {
+                $scope.numberError = false;
+            }
+        });
     }]);
 }());
