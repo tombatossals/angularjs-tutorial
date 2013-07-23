5635dbf98072551d06608c57834c1aa50e900a54
tombatossals <david.rubert@gmail.com>
2013-07-23 23:47:13 +0200

    fase3

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,46 +1,35 @@
 (function() {
     "use strict";
 
-    var app = angular.module("HelloWorldApp", [], function($routeProvider, $locationProvider) {
-        $routeProvider.when("/area", {
-            templateUrl: 'templates/area.html'
+    var app = angular.module("FlagQuizApp", []);
+
+    function cropAndMix(arr, size) {
+        arr.sort(function(a, b) {
+             return Math.random() - 0.5;
         });
 
-        $routeProvider.when("/perimeter", {
-            templateUrl: 'templates/perimeter.html'
+        return arr.splice(0, size);
+    }
+
+    app.controller("FlagQuizController", [ "$scope", "$http", function($scope, $http) {
+        $scope.gamestart = false;
+        $scope.selectedCountry = undefined;
+        $scope.selectedFlag = undefined;
+        $http.get("json/all.json").success(function(data, status) {
+            $scope.countries = cropAndMix(data, 20);
         });
 
-        $locationProvider.html5Mode(false);
-    });
-
-    app.controller("HelloWorldController", [ "$scope", "$route", "$routeParams", "$location", function($scope, $route, $routeParams, $location) {
-
-        $scope.$route = $route;
-        $scope.$location = $location;
-        $scope.$routeParams = $routeParams;
-        $scope.numberError = false;
-
-        $scope.circleArea = function() {
-            console.log($scope);
-            return Math.PI * Math.pow($scope.number, 2);
+        $scope.setCountry = function(country) {
+            $scope.selectedCountry = country;
         };
 
-        $scope.circleArea = function() {
-            return Math.PI * Math.pow($scope.number, 2);
+        $scope.setFlag = function(country) {
+            $scope.selectedFlag = country;
+            console.log(country);
         };
 
-        $scope.circlePerimeter = function() {
-            return 2 * Math.PI * $scope.number;
-        };
-
-        $scope.$watch("number", function(newValue, oldValue) {
-            if (newValue === undefined) return;
-
-            if (isNaN(newValue)) {
-                $scope.numberError = true;
-            } else {
-                $scope.numberError = false;
-            }
+        $scope.$watch("selectedCountry", function(country) {
+            console.log(country);
         });
     }]);
 
